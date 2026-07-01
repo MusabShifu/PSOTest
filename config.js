e159befc83e4764dfe268cfb9e54407882fd0fce
Musab Alhaj <108827728+MusabShifu@users.noreply.github.com>
2026-07-01 09:03:11 +0200

    Change group link 1/7/26

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // إعدادات البوت والمجموعة المستهدفة
 const BOT_TOKEN = "8671492484:AAEnZ7qF2AiiU_DUM67aHEt6D0gKFYpR6io";
 const CHAT_ID = "-1003953501820";
-const TELEGRAM_GROUP_LINK = "https://t.me/+sLcnx41Rxd4zZjNk";
+const TELEGRAM_GROUP_LINK = "https://t.me/+60JuAuMNzFs3NWJk";
 
 // دالة لتشفير HTML
 function escapeHTML(str) {
